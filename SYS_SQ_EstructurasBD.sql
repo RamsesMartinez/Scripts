22905cf03b7af7962d6e8442b7165fd89b3a807d
Ramses <ramses.mtz96@gmail.com>
2017-12-05 18:02:12 -0600

    Se agrega validación de tipo de campo en ItemCode de tabla ITL1

--- SYS_SQ_EstructurasBD.sql
+++ SYS_SQ_EstructurasBD.sql
@@ -36966,6 +36966,18 @@ ELSE
 			END
 		ELSE
 			BEGIN
+				--SE EVALÚA EL TIPO DE DATOS
+				IF
+				(SELECT 
+					DATA_TYPE
+				FROM INFORMATION_SCHEMA.COLUMNS
+				WHERE TABLE_NAME = 'ITL1' AND COLUMN_NAME = 'ItemCode'
+				AND TABLE_SCHEMA = @sEsquema AND TABLE_CATALOG = @sBase
+				) <> 'nvarchar'
+					BEGIN
+						--MODIFICAR EL TIPO DEL CAMPO
+						ALTER TABLE [dbo].[ITL1] ALTER COLUMN [ItemCode] [nvarchar](50) NOT NULL
+					END
 				--SE EVALÚA LA LONGITUD
 				IF ISNULL(COL_LENGTH('dbo.ITL1','ItemCode'),0) <> 100
 					BEGIN
